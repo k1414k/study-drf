acd39f7e45b2f0ebd63522f3838260b14dd368d5
k1414k <k14146k@naver.com>
2025-10-11 22:30:28 +0900

    init

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from .views import RegisterView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view())
+    # path('api-auth/', include('rest_framework.urls')),
+]
