4d0522ad78329a1a78195a2dc8f47a43240eb64b
k1414k <k14146k@naver.com>
2025-10-13 14:41:33 +0900

    仮装環境変更”

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from .views import RegisterView, LoginView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #settingと繋ぎ
